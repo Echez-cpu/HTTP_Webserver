4d6a8291ece6d1d38c9392a6c520416ba8dc3fa4
Patrick Echezona Okpalaeze <echez@MacBook-Pro-von-Patrick.local>
2025-07-10 16:37:35 +0200

    This is a very clean version

---
+++ webserv_mostResent copy1_macOS/basic_CGI/load_listing.php
@@ -0,0 +1,23 @@
+<?php
+    $file = dirname(dirname(__FILE__)) . '/d_struct/data.json';
+    $dataJson = file_get_contents($file);
+
+    $data = json_decode($dataJson, true);
+
+    $entries = $data['entries'];
+
+    
+    usort($entries, function($a, $b) {
+        return $a['id'] - $b['id'];
+    });
+
+    foreach ($entries as $entry) {
+        echo "<tr>";
+        echo "<td>" . $entry['id'] . "</td>";
+        echo "<td>" . $entry['description'] . "</td>";
+        echo "<td>" . $entry['price'] . "</td>";
+        echo "<td>" . $entry['phone'] . "</td>";
+        echo "<td> <img src='images/{$entry['id']}.jpeg' width='115' height='115' alt='car image'/>  </td>";
+        echo "</tr>";
+    }
+?>
